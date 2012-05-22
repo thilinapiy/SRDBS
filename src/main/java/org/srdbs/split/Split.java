0a14d0afe5019b4ec0518a6b8b05caf0b291ab24
Thilina Piyasundara <mail@thilina.org>
2012-05-23 04:40:35 +0530

    Add Split module.

---
+++ src/main/java/org/srdbs/split/Split.java
@@ -0,0 +1,22 @@
+package org.srdbs.split;
+
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/**
+ * Split files.
+ *
+ * @author Thilina Piyasundara
+ * @version 0.1
+ */
+public class Split {
+
+    public static Logger logger = Logger.getLogger("systemsLog");
+
+
+}
