a1ac1216228e869cd5eabb8efd880df386150247
Thilina Piyasundara <mail@thilina.org>
2012-06-05 14:37:23 +0530

    Add Raid to 3 clouds.

--- src/main/java/org/srdbs/core/RunBackup.java
+++ src/main/java/org/srdbs/core/RunBackup.java
@@ -1,9 +1,11 @@
 package org.srdbs.core;
 
 import org.apache.log4j.Logger;
+import org.srdbs.sftp.Sftp;
 import org.srdbs.split.FileData;
 import org.srdbs.split.MYSpFile;
 import org.srdbs.split.MyFile;
+import org.srdbs.split.Split;
 
 import java.util.List;
 
@@ -29,8 +31,6 @@ public class RunBackup {
         int noOfFiles = 0;
         List<MyFile> listOfFiles = null;
 
-        logger.info("Running the raid.");
-
         listOfFiles = FileData.Read(path);
 
         for (MyFile file : listOfFiles) {
@@ -40,6 +40,54 @@ public class RunBackup {
                     + path + Global.fs + file.getName()
                     + " in to " + count + " parts.");
             noOfFiles++;
+
+            // RAID
+            int[] raidArray = Sftp.raid(7, 3, 2, 3, 2);
+            //TODO remove this
+
+            for (int j = 0; j < raidArray.length; ) {
+                logger.info("Packet " + (j + 2) / 2 + ": Orignal: " + raidArray[j] + " , " + "Raid: " + raidArray[j + 1]);
+                j = j + 2;
+            }
+
+            int ftpFileNo = 1;
+            boolean raidDone = false;
+            for (int j = 0; j < raidArray.length; j++) {
+                if (raidArray[j] == 1) {
+                    logger.info("Uploading " + file + " to cloud 1.");
+                    Sftp.upload("192.168.222.141", "prabodha", "prabodha", 22, "/home/prabodha",
+                            dest + Global.fs + file.getName() + Split.createSuffix(ftpFileNo));
+                }
+
+                if (raidArray[j] == 2) {
+                    logger.info("Uploading " + file + " to cloud 2.");
+                    Sftp.upload("192.168.222.142", "prabodha", "prabodha", 22, "/home/prabodha",
+                            dest + Global.fs + file.getName() + Split.createSuffix(ftpFileNo));
+                }
+
+                if (raidArray[j] == 3) {
+                    logger.info("Uploading " + file + " to cloud 3.");
+                    Sftp.upload("192.168.222.143", "prabodha", "prabodha", 22, "/home/prabodha",
+                            dest + Global.fs + file.getName() + Split.createSuffix(ftpFileNo));
+                }
+
+                if (raidArray[j] == 4) {
+                    logger.info("Uploading " + file + " to cloud 4.");
+                    //  Sftp.upload()
+                }
+
+                if (raidArray[j] == 5) {
+                    logger.info("Uploading " + file + " to cloud 5.");
+                    //  Sftp.upload()
+                }
+                if (raidDone) {
+                    ftpFileNo++;
+                    raidDone = false;
+                } else
+                    raidDone = true;
+            }
+            // FTP data to server.
+
         }
 
         try {
