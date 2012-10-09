2d29b4440a61449ca49f8498a5e3f6de8d53b044
chana885391 <chanaka.yapa@gmail.com>
2012-10-09 14:00:52 +0530

    Hash Comparing error fix in count

--- src/main/java/org/srdbs/core/RunRestore.java
+++ src/main/java/org/srdbs/core/RunRestore.java
@@ -300,6 +300,7 @@ public class RunRestore {
 
                             Check = true;
                             restoreLog.info("Pass : " + myfile.getName());
+                            count++;
 
                         }   else {
 
